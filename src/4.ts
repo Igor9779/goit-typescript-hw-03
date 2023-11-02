688a7c66dd9625060dca6b5d7807e3e734ab8e94
Igor9779 <darthhoit97@gmail.com>
2023-11-02 19:23:48 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,61 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+};
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+};
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    private tenants: Person[] = [];
+
+
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+    };
+
+    comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log('You entered the house');
+        } else {
+            console.log('The doors are closed');
+        };
+    };
+
+    abstract openDoor(key: Key): void;
+};
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The doors are open.');
+    } else {
+      console.log('Wrong key. The doors remain closed.');
+    }
+  }
+};
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +64,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
